0989a853422c8b7ac9b68874b46f952d3343abe8
Csavu-art <joonakha@metropolia.fi>
2026-04-02 10:52:16 +0300

    Lisätty Flask test

--- Flask/FlaskTest.py
+++ Flask/FlaskTest.py
@@ -1,15 +1,40 @@
-from flask import Flask, request
+from flask import Flask, Response
+import json
 
 app = Flask(__name__)
-@app.route('/summa')
-def summa():
+@app.route('/summa/<luku1>/<luku2>')
+def summa(luku1, luku2):
+    try:
+        luku1 = float(luku1)
+        luku2 = float(luku2)
+        summa = luku1 + luku2
 
-    #Haetaan parametrien arvot
-    args = request.args
-    luku1 = float(args.get("luku1"))
-    luku2 = float(args.get("luku2"))
+        tilakoodi = 200
+        vastaus = {
+            "status": tilakoodi,
+            "luku1": luku1,
+            "luku2": luku2,
+            "summa": summa
+        }
 
-    summa = luku1 + luku2
-    return str(summa)
+    except ValueError:
+        tilakoodi = 400
+        vastaus = {
+            "status": tilakoodi,
+            "teksti": "Virheellinen yhteenlaskettava"
+        }
 
-app.run(use_reloader=True, host='127.0.0.1', port=3000)
+    json_vastaus = json.dumps(vastaus)
+    return Response(response=json_vastaus, status=tilakoodi, mimetype="application/json")
+
+@app.errorhandler(404)
+def page_not_found(virhe):
+    vastaus = {
+        "status": "404",
+        "teksti": "Virheellinen päätepiste"
+    }
+    jsonvast = json.dumps(vastaus)
+    return Response(response=jsonvast, status=404, mimetype="application/json")
+
+if __name__ == '__main__':
+    app.run(use_reloader=True, host='127.0.0.1', port=3000)
