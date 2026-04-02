817cec16fe23cabcdd1e45fb6ec64af90a37de04
Csavu-art <joonakha@metropolia.fi>
2026-04-02 11:18:12 +0300

    Lisätty Flask t13.1

--- Flask/teht_13.1.py
+++ Flask/teht_13.1.py
@@ -2,51 +2,22 @@ from flask import Flask, Response
 import json
 
 app = Flask(__name__)
-
-
-def onko_alkuluku(luku):
-    if luku < 2:
-        return False
-
-    for i in range(2, luku):
-        if luku % i == 0:
-            return False
-
-    return True
-
-
 @app.route('/alkuluku/<luku>')
 def alkuluku(luku):
-    try:
-        luku = int(luku)
-        tulos = onko_alkuluku(luku)
 
-        tilakoodi = 200
-        vastaus = {
-            "Number": luku,
-            "isPrime": tulos
-        }
+    kokonaisluku = int(luku)
+    onko_alkuluku = True
+    for x in range(2, kokonaisluku):
+        if kokonaisluku % x == 0:
+            onko_alkuluku = False
+            break
 
-    except ValueError:
-        tilakoodi = 400
-        vastaus = {
-            "status": tilakoodi,
-            "teksti": "Parametri ei ole kokonaisluku"
-        }
-
-    json_vastaus = json.dumps(vastaus)
-    return Response(response=json_vastaus, status=tilakoodi, mimetype="application/json")
-
-
-@app.errorhandler(404)
-def page_not_found(virhe):
     vastaus = {
-        "status": 404,
-        "teksti": "Virheellinen päätepiste"
+        "Number" : luku,
+        "IsPrime" : onko_alkuluku
     }
-    json_vastaus = json.dumps(vastaus)
-    return Response(response=json_vastaus, status=404, mimetype="application/json")
+
+    return vastaus
 
 
-if __name__ == '__main__':
-    app.run(use_reloader=True, host='127.0.0.1', port=3000)
+app.run(use_reloader=True, host='127.0.0.1', port=3000)
